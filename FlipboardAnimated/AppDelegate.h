8e67c1f0bb1a1ada8847649921b9fc2793ed5138
hechaohe <chao163hc@163.com>
2018-03-20 11:28:06 +0800

    Initial Commit

---
+++ FlipboardAnimated/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlipboardAnimated
+//
+//  Created by 贺超 on 2018/3/20.
+//  Copyright © 2018年 贺超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlipboardAnimated/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FlipboardAnimated
+//
+//  Created by 贺超 on 2018/3/20.
+//  Copyright © 2018年 贺超. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
